876a5a807df398d7682b6ad5444a908316b14c6f
„klepo313” <antonio.klepo1@gmail.com>
2024-09-08 13:31:52 +0200

    dodana validacija statusa

--- service/checkLogin.js
+++ service/checkLogin.js
@@ -7,23 +7,34 @@ const checkLogin = async () => {
 
         const response = await axios.get(url, {
             withCredentials: true,
+            validateStatus: function (status) {
+                console.log('is_loggedin: ', status)
+                return status === 200 || status === 401;
+            }
         });
 
-        console.log("Logiran? ", response.data)
+        console.log("Logiran? ", response.data.isLoggedin, response.status);
 
-        if (response.status == 200) {
-            return response.data;
-        } else {
-            console.error('Došlo je do pogreške na serveru!');
-            return null; // Vratite null ako je greška
+        // Ako je status 200, vraćamo isLoggedin vrijednost
+        if (response.status === 200) {
+            return response.data.isLoggedin;
         }
 
-    } catch (error) {
-        console.error('Greška pri provjeri logiranja: ' + error)
+        // Ako je status 401 (Unauthorized), tretiramo kao da korisnik nije prijavljen
+        if (response.status === 401) {
+            return 0;
+        }
+
+        // Ako dođe do drugih statusnih kodova, vraćamo null kao grešku
         return null;
+
+    } catch (error) {
+        // Ako se dogodi neka druga greška (npr. mrežna greška), vraćamo 0
+        console.error('Greška pri provjeri logiranja: ' + error);
+        return 0;
     }
-}
+};
 
 export {
     checkLogin
-}
+};
