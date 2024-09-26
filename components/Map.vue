a3bd0703668ece5bd9c9c6da3e85963fd17303a5
„klepo313” <antonio.klepo1@gmail.com>
2024-09-27 00:07:02 +0200

    dodana uređena zemlja

--- components/Map.vue
+++ components/Map.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div id="map" style="height: 500px;"></div>
 </template>
 
@@ -73,5 +73,46 @@ onMounted(async () => {
 <style scoped>
 #map {
     height: 500px;
+}
+</style> -->
+
+<template>
+    <div>
+        <div>
+            <!-- Radio buttoni za odabir mape -->
+            <label>
+                <input type="radio" value="https://oss.uredjenazemlja.hr/map" v-model="selectedUrl">
+                Uređena Zemlja
+            </label>
+            <label>
+                <input type="radio" value="https://geoportal.dgu.hr/" v-model="selectedUrl">
+                Geoportal
+            </label>
+        </div>
+
+        <!-- Iframe koji prikazuje mapu prema odabranom URL-u -->
+        <iframe :src="selectedUrl" style="width: 100%; height: 600px; border: none;" />
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// Početni URL koji će biti prikazan
+const selectedUrl = ref('https://oss.uredjenazemlja.hr/map');
+</script>
+
+<style scoped>
+h3 {
+    margin-bottom: 10px;
+}
+
+label {
+    margin-right: 15px;
+    font-weight: bold;
+}
+
+iframe {
+    margin-top: 20px;
 }
 </style>
