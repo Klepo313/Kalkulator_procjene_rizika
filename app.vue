8197164a31e13160bb01be3bc6244281e075a99e
„klepo313” <antonio.klepo1@gmail.com>
2024-09-28 00:09:01 +0200

    dodan refresh token

--- app.vue
+++ app.vue
@@ -6,6 +6,44 @@
 
 <script setup>
 
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { setupTokenRefresh } from './utils/refreshToken';
+
+const userActive = ref(false); // Praćenje aktivnosti korisnika
+let activityTimeout;
+
+function resetActivityTimer() {
+  userActive.value = true;
+
+  // Resetiraj timer ako je korisnik aktivan
+  clearTimeout(activityTimeout);
+
+  // Ako nema aktivnosti 5 minuta, postavi userActive na false
+  activityTimeout = setTimeout(() => {
+    userActive.value = false;
+  }, 5 * 60 * 1000); // 5 minuta neaktivnosti
+}
+
+onMounted(() => {
+  // Dodaj event listenere za praćenje korisničke aktivnosti
+  window.addEventListener('mousemove', resetActivityTimer);
+  window.addEventListener('keydown', resetActivityTimer);
+  window.addEventListener('scroll', resetActivityTimer);
+
+  // Pokreni provjeru i potencijalno osvježavanje sesije
+  setupTokenRefresh();
+});
+
+onBeforeUnmount(() => {
+  // Ukloni event listenere kada se komponenta uništava
+  window.removeEventListener('mousemove', resetActivityTimer);
+  window.removeEventListener('keydown', resetActivityTimer);
+  window.removeEventListener('scroll', resetActivityTimer);
+
+  // Očisti timer
+  clearTimeout(activityTimeout);
+});
+
 const title = ref('Kalkulator procjene rizika')
 const description = ref('Kalkulator procjene rizika')
 
