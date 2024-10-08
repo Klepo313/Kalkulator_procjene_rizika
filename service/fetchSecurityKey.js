b742254acc78002d3bcc04fce37469d1ff73daad
„klepo313” <antonio.klepo1@gmail.com>
2024-10-09 00:48:13 +0200

    dodan ispis responsea

--- service/fetchSecurityKey.js
+++ service/fetchSecurityKey.js
@@ -4,6 +4,7 @@ import { base_url } from "#imports";
 const getSecKey = async () => {
     try {
         const response = await axios.get(`${base_url}/user/secKey`);
+        console.log("Response:", response.data);
         return response.data.secretKey;
     } catch (error) {
         console.error('Error fetching secure data:', error);
