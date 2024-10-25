da9583dc2dd2e2d9923f2ab71d5743cbff7b878c
„klepo313” <antonio.klepo1@gmail.com>
2024-10-25 16:41:25 +0200

    uklonjeno dodavanje +1 dana

--- utils/dateFormatter.js
+++ utils/dateFormatter.js
@@ -3,8 +3,8 @@ export function formatDateToDMY(dateString, delimiter) {
     const date = new Date(dateString);
 
     // Get day, month, and year
-    const day = String(date.getUTCDate() + 1).padStart(2, '0'); // pad with leading zero if needed
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // January is 0!
+    const day = String(date.getUTCDate()).padStart(2, '0'); // pad with leading zero if needed
+    const month = String(date.getUTCMonth()).padStart(2, '0'); // January is 0!
     const year = date.getUTCFullYear();
 
     if (delimiter == '.') {
@@ -29,3 +29,17 @@ export function formatDateToISO(date) {
 
     return date.toISOString();
 }
+
+export function getYearsRange(datod, datdo) {
+    const startYear = new Date(datod).getUTCFullYear();
+    const endYear = new Date(datdo).getUTCFullYear();
+
+    // Ako je početna i krajnja godina ista, prikaži samo jednu godinu
+    if (startYear === endYear) {
+        return `${startYear}`;
+    }
+
+    // Ako su različite, prikaži raspon godina
+    return `${startYear} - ${endYear}`;
+}
+
