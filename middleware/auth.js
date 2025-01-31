605069b96012ab07edecff2dace6e66cb3a80413
„klepo313” <antonio.klepo1@gmail.com>
2025-01-31 13:34:16 +0100

    uklonjen auth client middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,42 @@
 // middleware/auth.js
-import { defineNuxtRouteMiddleware, navigateTo, useCookie } from '#app';
+// middleware/auth.js
+import { defineNuxtRouteMiddleware, navigateTo } from '#app';
 
-export default defineNuxtRouteMiddleware((to) => {
-    const token = useCookie('userToken');
+export default defineNuxtRouteMiddleware(async () => {
+    // const apiBase = useRuntimeConfig().public.baseUrl;
 
-    // Ako korisnik nije prijavljen
-    if (!token.value) {
-        // Sačuvaj trenutnu rutu u query parametar `redirectTo`
-        return navigateTo(`/login?redirectTo=${encodeURIComponent(to.fullPath)}`);
-    }
+    // const checkLogin = async () => {
+    //     try {
+    //         const response = await $fetch(`${apiBase}/user/is_loggedin`, {
+    //             method: 'GET',
+    //             credentials: 'include',
+    //         });
+    //         return response; // Pretpostavka da vraća { isLoggedin: true/false }
+    //     } catch (error) {
+    //         console.error("Greška kod provjere prijave:", error);
+    //         return { isLoggedin: false }; // Osiguraj fallback
+    //     }
+    // };
 
-    console.log("User logged in");
+    // const response = await checkLogin();
+    // console.log("Provjera prijave:", response);
+
+    // if (!response || !response.isLoggedin) {
+    //     return navigateTo('/login');
+    // }
 });
+
+
+
+
+// export default defineNuxtRouteMiddleware((to) => {
+//     const token = useCookie('userToken');
+
+//     // Ako korisnik nije prijavljen
+//     if (!token.value) {
+//         // Sačuvaj trenutnu rutu u query parametar `redirectTo`
+//         return navigateTo(`/login?redirectTo=${encodeURIComponent(to.fullPath)}`);
+//     }
+
+//     console.log("User logged in");
+// });
