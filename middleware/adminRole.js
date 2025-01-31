0f90aa1d6890a6a09d4815f662b54abb49a383f3
„klepo313” <antonio.klepo1@gmail.com>
2025-01-31 13:35:59 +0100

    dodana prava na korištenje administratora

--- middleware/adminRole.js
+++ middleware/adminRole.js
@@ -1,4 +1,14 @@
-// // middleware/adminRole.js
-// export default defineNuxtRouteMiddleware((to, from) => {
-//     // middleware/adminRole
-// });
+// middleware/auth-role.global.ts
+import { defineNuxtRouteMiddleware, navigateTo } from "#app";
+import { useAuthStore } from "~/stores/auth";
+
+export default defineNuxtRouteMiddleware(() => {
+    const authStore = useAuthStore();
+
+    // Ako korisnik nema definirane role ili nijedna ne počinje s "AD", preusmjeri na "/"
+    const hasAccess = authStore.userRoles.some(role => role.startsWith("AD"));
+
+    if (!hasAccess) {
+        return navigateTo("/");
+    }
+});
