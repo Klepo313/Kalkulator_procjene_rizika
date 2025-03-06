55cfd5200ccc8230556be1d28434c37ff068738c
„klepo313” <antonio.klepo1@gmail.com>
2025-03-06 14:37:13 +0100

    dodana getReport funkcija za dodavanje izvještaja

--- service/kesp/fetchHeader.ts
+++ service/kesp/fetchHeader.ts
@@ -20,3 +20,28 @@ export const getHeader = async (id: number | string): Promise<unknown> => {
         return null;
     }
 };
+
+export async function getReport(id: number | string): Promise<Blob | null> {
+    if (!id) return null;
+
+    const { $api } = useNuxtApp();
+    const url = `CO2/header/${id}/report`;
+
+    try {
+        const response = await $api.get(url, {
+            Headers: { 'Content-Type': 'application/pdf' },
+            responseType: 'blob', // Dohvaćamo kao binarni podatak
+        });
+
+        if (response.status === 200) {
+            return response.data; // Vraćamo Blob
+        } else {
+            console.error('Došlo je do pogreške na serveru!');
+            return null;
+        }
+    } catch (error) {
+        console.error('Greška pri dohvaćanju izvještaja:', error);
+        return null;
+    }
+}
+
