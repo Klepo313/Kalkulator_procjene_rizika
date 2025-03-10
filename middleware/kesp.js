3e4b009014107ff9d0757e5fdff558cda3815191
„klepo313” <antonio.klepo1@gmail.com>
2025-03-10 21:35:28 +0100

    dodana provjera jwt tokena (nedovršeno)

--- middleware/kesp.js
+++ middleware/kesp.js
@@ -1,8 +1,9 @@
 // middleware/kesp.js
+
 export default defineNuxtRouteMiddleware((to) => {
     try {
         const id = to.query.id;
-
+        
         console.log(to.query, id, !id, id !== 'null'); // Debug log za provjeru vrijednosti
 
         if (!id || (id.length < 60 && id !== 'null')) {
@@ -13,6 +14,44 @@ export default defineNuxtRouteMiddleware((to) => {
         return navigateTo('/kesp/predlosci');
     }
 });
+
+
+// import { jwtDecode } from "jwt-decode";
+// export default defineNuxtRouteMiddleware((to) => {
+//     try {
+//         const id = to.query.id;
+
+//         console.log("Provjera ID-a:", id);
+
+//         // Ako ID ne postoji ili je prekratak, preusmjeri
+//         if (!id || id.length < 50) {
+//             console.warn("Nevažeći JWT, preusmjeravam na /kesp/predlosci");
+//             return navigateTo('/kesp/predlosci');
+//         }
+
+//         try {
+//             // Dekodiranje JWT tokena
+//             const decoded = jwtDecode(id);
+
+//             // Osnovna validacija
+//             if (!decoded || typeof decoded !== "object") {
+//                 throw new Error("Nevažeći JWT format");
+//             }
+
+//             console.log("Dekodirani JWT:", decoded);
+//         } catch (error) {
+//             console.error("Neuspjelo dekodiranje JWT-a:", error);
+//             return navigateTo('/kesp/predlosci');
+//         }
+
+//     } catch (error) {
+//         console.error('Middleware error:', error);
+//         return navigateTo('/kesp/predlosci');
+//     }
+// });
+
+
+
 
 // export default defineNuxtRouteMiddleware((to) => {
 //     const urlParams = new URLSearchParams(to.query);
