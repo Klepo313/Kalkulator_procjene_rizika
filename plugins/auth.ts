a1dae2d1bfd16a3bca20762a4c90178af6d9c002
„klepo313” <antonio.klepo1@gmail.com>
2025-03-18 16:02:41 +0100

    dodana provjera klijentske i serverske strane kod pokretanja plugina

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -3,22 +3,31 @@ import { useAuthStore } from "~/stores/auth";
 import { useRoute } from "#app";
 
 export default defineNuxtPlugin(async (nuxtApp) => {
-    const authStore = useAuthStore();
-    const route = useRoute(); // Dohvaćamo trenutnu rutu
+  const authStore = useAuthStore();
+  const route = useRoute(); // Dohvaćamo trenutnu rutu
 
-    // Ako je korisnik na /login stranici, ne radimo provjere autentifikacije
-    if (route.path === "/login") {
-        // console.log("Na login stranici - preskačemo auth provjere.");
-        return;
-    }
+  if (route.path === "/login") {
+    // Na login stranici preskačemo provjere
+    return;
+  }
 
-    console.log("Provjera autentifikacije...", authStore.isLoggedin);
-
+  // Ako smo na serveru, koristimo podatke iz SSR konteksta
+  if (import.meta.server) {
+    // console.log("Server Provjera autentifikacije...", authStore.isLoggedin);
     if (nuxtApp.ssrContext?.event.context.isLoggedin !== undefined) {
-        authStore.isLoggedin = nuxtApp.ssrContext.event.context.isLoggedin;
-        authStore.userRoles = nuxtApp.ssrContext.event.context.userRoles || [];
-        authStore.exp = nuxtApp.ssrContext.event.context.exp || null;
-    } else {
-        authStore.checkAuth();
+    //   console.log("Server UŠLO 1");
+      authStore.isLoggedin = nuxtApp.ssrContext.event.context.isLoggedin;
+      authStore.userRoles = nuxtApp.ssrContext.event.context.userRoles || [];
+      authStore.exp = nuxtApp.ssrContext.event.context.exp || null;
     }
+  }
+  
+  // Na klijentskoj strani, ako je potrebno, izvrši dodatnu provjeru samo ako stanje nije već postavljeno
+  if (import.meta.client) {
+    // console.log("Client Provjera autentifikacije...", authStore.isLoggedin);
+    if (!authStore.isLoggedin) {
+      authStore.checkAuth();
+    //   console.log("Client UŠLO 2");
+    }
+  }
 });
