a2d2a29cd86ae2ad77034e21120b2f6cf5983e24
„klepo313” <antonio.klepo1@gmail.com>
2025-04-03 23:14:54 +0200

    uklonjene višak datoteke

--- utils/generateCSRFtoken.js
+++
@@ -1,9 +0,0 @@
-const generateCsrfToken = () => {
-    const array = new Uint8Array(32); // 32 bytes = 256 bits
-    window.crypto.getRandomValues(array);
-    return Array.from(array, byte => byte.toString(16).padStart(2, '0')).join('');
-};
-
-export {
-    generateCsrfToken,
-};
