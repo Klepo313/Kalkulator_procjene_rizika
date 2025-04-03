52624f8238390a6b955b917f8b6e7c84ce3f4962
„klepo313” <antonio.klepo1@gmail.com>
2025-04-03 23:31:23 +0200

    ažuriranje sidebara

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,102 +1,99 @@
 <template>
-    <div class="body">
-        <div :class="['sidebar', { 'collapsed': isCollapsed }]" ref="sidebar">
-            <AdminSidebar :is-collapsed="isCollapsed" @toggle-sidebar="toggleSidebar" />
-        </div>
-        <main :style="mainStyles" style="background-color: white;">
-            <div class="main-content">
-                <NuxtPage />
-                <!-- <p>Prikaz stranice</p> -->
-            </div>
-        </main>
+  <div class="body">
+    <div :class="['sidebar', { collapsed: isCollapsed }]" ref="sidebar">
+      <AdminSidebar
+        :is-collapsed="isCollapsed"
+        @toggle-sidebar="toggleSidebar"
+      />
     </div>
+    <main :style="mainStyles" style="background-color: white">
+      <div class="main-content">
+        <NuxtPage />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script setup>
-import {
-    ref,
-    computed,
-    watch
-} from 'vue'
-import AdminSidebar from '~/components/AdminSidebar.vue';
+import { ref, computed, watch, onMounted } from "vue";
+import AdminSidebar from "~/components/admin/AdminSidebar.vue";
 
-definePageMeta({
-    middleware: [
-        'auth',
-        'admin-role'
-    ],
-})
+// Inicijalno postavljamo default vrijednost
+const isCollapsed = ref(false);
+const sidebarWidth = ref("275px");
 
-// Inicijaliziramo isCollapsed na temelju vrijednosti spremljene u localStorage (ako postoji)
-const storedCollapsed = import.meta.client ? localStorage.getItem('isCollapsed') : false;
-const isCollapsed = ref(storedCollapsed ? JSON.parse(storedCollapsed) : false);
+// Dohvaćamo vrijednost iz localStorage samo na klijentskoj strani
+onMounted(() => {
+  const storedCollapsed = localStorage.getItem("isCollapsed");
+  if (storedCollapsed !== null) {
+    isCollapsed.value = JSON.parse(storedCollapsed);
+  }
+  sidebarWidth.value = isCollapsed.value ? "70px" : "275px";
+});
 
-// Postavljamo početnu širinu sidebar-a ovisno o isCollapsed vrijednosti
-const sidebarWidth = ref(isCollapsed.value ? '70px' : '275px');
-
-// Funkcija za prebacivanje stanja bočne trake
+// Funkcija za prebacivanje stanja sidebara
 const toggleSidebar = () => {
-    isCollapsed.value = !isCollapsed.value;
+  isCollapsed.value = !isCollapsed.value;
 };
 
-// Reaktivno praćenje promjena u isCollapsed za ažuriranje širine sidebarWidth i spremanje u localStorage
+// Praćenje promjena i spremanje nove vrijednosti u localStorage
 watch(isCollapsed, (newValue) => {
-    sidebarWidth.value = newValue ? '70px' : '275px';
-    localStorage.setItem('isCollapsed', JSON.stringify(newValue));
+  sidebarWidth.value = newValue ? "70px" : "275px";
+  localStorage.setItem("isCollapsed", JSON.stringify(newValue));
 });
 
 // Kompjuterana vrijednost za stilove main elementa
 const mainStyles = computed(() => ({
-    marginLeft: sidebarWidth.value,
-    width: `calc(100% - ${sidebarWidth.value})`,
+  marginLeft: sidebarWidth.value,
+  width: `calc(100% - ${sidebarWidth.value})`,
 }));
 </script>
 
 <style scoped>
 .body {
-    display: flex;
-    gap: 25px;
-    overflow: visible;
-    background-color: var(--admin-bg-color);
+  display: flex;
+  gap: 25px;
+  overflow: visible;
+  background-color: var(--admin-bg-color);
 }
 
 .sidebar {
-    position: fixed;
-    width: 275px;
-    /* Početna širina */
-    height: 100vh;
-    top: 0;
-    left: 0;
-    overflow-y: auto;
-    transition: width 0.3s ease;
-    background: none;
-    /* Glatka animacija promjene širine */
+  position: fixed;
+  width: 275px;
+  /* Početna širina */
+  height: 100vh;
+  top: 0;
+  left: 0;
+  overflow-y: auto;
+  transition: width 0.3s ease;
+  background: none;
+  /* Glatka animacija promjene širine */
 }
 
 .sidebar.collapsed {
-    width: 70px;
-    /* Širina kad je suženo */
+  width: 70px;
+  /* Širina kad je suženo */
 }
 
 main {
-    padding: 13px 13px 13px 0px;
-    min-height: 100vh;
-    z-index: 99;
-    overflow: visible;
-    overflow-y: auto;
-    transition: margin-left 0.3s ease, width 0.3s ease;
-    background-color: var(--admin-bg-color) !important;
+  padding: 13px 13px 13px 0px;
+  min-height: 100vh;
+  z-index: 99;
+  overflow: visible;
+  overflow-y: auto;
+  transition: margin-left 0.3s ease, width 0.3s ease;
+  background-color: var(--admin-bg-color) !important;
 }
 
 .main-content {
-    height: 100%;
-    width: 100%;
-    background-color: var(--bg-layout-color);
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    transition: background-color 0.3s ease;
-    overflow: visible;
-    padding: 34px 26px 26px 26px;
-    z-index: 99;
+  height: 100%;
+  width: 100%;
+  background-color: var(--bg-layout-color);
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s ease;
+  overflow: visible;
+  padding: 34px 26px 26px 26px;
+  z-index: 99;
 }
 </style>
