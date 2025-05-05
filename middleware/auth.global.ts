eac4d39590ed88e3c55aa3fd7232b93481b3a5f2
„klepo313” <antonio.klepo1@gmail.com>
2025-05-05 20:10:33 +0200

    test auth middlewarea sa api pozivom

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,27 +1,27 @@
 // /middleware/auth.global.ts
-export default defineNuxtRouteMiddleware((to, from) => {
-    const userStore = useUserStore();
-
-    if (to.path === "/login") return;
-    
-    if (!userStore.isLoggedin) {
-        return navigateTo(`/login?redirectTo=${encodeURIComponent(to.fullPath)}`);
-    }
-});
-
-// export default defineNuxtRouteMiddleware(async (to, from) => {
-//     // Preskoči provjeru ako je ruta /login
-//     if (to.path === "/login") return;
-
-//     // Dinamički importiraj funkciju za provjeru autentifikacije
-//     const { checkAuth } = await import("~/composables/useAuth");
-
-//     // Čekaj da se provjera autentifikacije završi
-//     await checkAuth();
-
-//     // Opcionalno: ako korisnik nije autentificiran, možeš preusmjeriti na login
+// export default defineNuxtRouteMiddleware((to, from) => {
 //     const userStore = useUserStore();
+
+//     if (to.path === "/login") return;
+    
 //     if (!userStore.isLoggedin) {
 //         return navigateTo(`/login?redirectTo=${encodeURIComponent(to.fullPath)}`);
 //     }
 // });
+
+export default defineNuxtRouteMiddleware(async (to, from) => {
+    // Preskoči provjeru ako je ruta /login
+    if (to.path === "/login") return;
+
+    // Dinamički importiraj funkciju za provjeru autentifikacije
+    const { checkAuth } = await import("~/composables/useAuth");
+
+    // Čekaj da se provjera autentifikacije završi
+    await checkAuth();
+
+    // Opcionalno: ako korisnik nije autentificiran, možeš preusmjeriti na login
+    const userStore = useUserStore();
+    if (!userStore.isLoggedin) {
+        return navigateTo(`/login?redirectTo=${encodeURIComponent(to.fullPath)}`);
+    }
+});
